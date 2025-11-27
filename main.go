69f84d5d17e4930d96443fef0e9919a9e04ebcb3
Oluwafikayo Ajayi <o.oluwafikayoajayi@gmail.com>
2025-11-27 13:25:51 +0100

    fetch all recipes from a json file

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
 	"time"
 
@@ -20,6 +22,9 @@ var recipes []Recipe
 
 func init() {
 	recipes = make([]Recipe, 0)
+	file, _ := ioutil.ReadFile("recipes.json")
+	_ = json.Unmarshal([]byte(file), &recipes)
+
 }
 
 func NewRecipeHandler(c *gin.Context) {
@@ -37,6 +42,10 @@ func NewRecipeHandler(c *gin.Context) {
 
 }
 
+func ListRecipesHandler (c *gin.Context){
+	c.JSON(http.StatusOK, gin.H{"data": recipes})
+}
+
 func main() {
 	router := gin.Default()
 
@@ -45,6 +54,7 @@ func main() {
 	})
 
 	router.POST("/recipes", NewRecipeHandler)
+	router.GET("/recipes", ListRecipesHandler)
 
 	router.Run(":8009")
 }
