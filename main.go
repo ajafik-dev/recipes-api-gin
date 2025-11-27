3a73094dbe9106f1986a44f0ef495092a45e4384
Oluwafikayo Ajayi <o.oluwafikayoajayi@gmail.com>
2025-11-27 13:07:31 +0100

    added endpoint to add recipe

--- main.go
+++ main.go
@@ -8,11 +8,33 @@ import (
 )
 
 type Recipe struct {
+	ID           string    `json:"id"`
 	Name         string    `json:"name"`
 	Tags         []string  `json:"tags"`
 	Ingredients  []string  `json:"ingredients"`
 	Instructions []string  `json:"instructions"`
 	PublishedAt  time.Time `json:"publishedAt"`
+}
+
+var recipes []Recipe
+
+func init() {
+	recipes = make([]Recipe, 0)
+}
+
+func NewRecipeHandler(c *gin.Context) {
+
+	var recipe Recipe
+	if err := c.ShoudBindJSON(&recipe); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	recipe.ID = xid.New().String()
+	recipe.PublishedAt = time.Now()
+	recipes = append(recipes, recipe)
+	c.JSON(http.StatusOK, recipe)
+
 }
 
 func main() {
@@ -21,6 +43,8 @@ func main() {
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"message": "Welcome to Recipes API"})
 	})
+
+	router.POST("/recipes", NewRecipeHandler)
 
 	router.Run(":8009")
 }
